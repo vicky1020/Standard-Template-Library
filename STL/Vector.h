50832fc7b68a689c5a2945ff8e5eec7098ae7104
vicky1020 <vivekkr1020@gmailcom>
2020-08-14 22:12:15 +0530

    adding realloc() and overloading operator like [] =

--- STL/Vector.h
+++ STL/Vector.h
@@ -1,28 +1,45 @@
 #pragma once
+#include "pch.h"
 
 template<typename T>
 class vector {
-	T* s;
-	int size_a = 0;
-	std::allocator<T> alloc;
+	T* vec = nullptr;
+	int size_v = 0;
+	int capacity_v = 2;
+	
+	void realloc(int new_size) {
+		T* new_vec = new T[new_size];
+		for (int i = 0; i < size_v; i++) {
+			new_vec[i] = vec[i];
+		}
+		delete[] vec;
+		vec = new_vec; 
+		capacity_v = new_size;
+	}
 public:
-	vector() :s(alloc.allocate(1)) {
-
+	vector() {
+		realloc(capacity_v);
 	}
 
 	void push_back(T val) {
-		s[size_a] = val;
-		size_a++;
+		if (size_v >= capacity_v) {
+			realloc(size_v + size_v / 2);
+		}
+		vec[size_v] = val;
+		size_v++;
 	}
 
-	T operator[](int index) {
-		if (index <= size_a) {
-			return s[index];
-		}
-		else return NULL;
+	const T& const operator[](int index) const {
+		assert(index <= size_v, "Index out of range!");
+		return vec[index];
+	}
+
+	T& operator[](int index) {
+		assert(index <= size_v, "Index out of range!");
+		return vec[index];
 	}
 
 	int size() {
-		return size_a;
+		return size_v;
 	}
 };
--- TestProject/Testing.cpp
+++ TestProject/Testing.cpp
@@ -6,6 +6,13 @@ int main() {
 	vec.push_back(1);
 	vec.push_back(2);
 	vec.push_back(3);
+	vec.push_back(4);
+	vec.push_back(5);
+	vec.push_back(6);
+	vec.push_back(7);
+	vec.push_back(8);
+	vec.push_back(9);
+	vec.push_back(0);
 	for (int i = 0; i < vec.size(); i++) {
 		std::cout<<vec[i]<<std::endl;
 	}
